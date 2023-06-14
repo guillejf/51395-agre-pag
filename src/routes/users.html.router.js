de429e3bc21d68406ee6021b110f32dd95a7240f
guillejf <maildeguille@gmail.com>
2023-06-13 21:15:46 -0300

    v1

--- src/routes/users.html.router.js
+++ src/routes/users.html.router.js
@@ -6,7 +6,12 @@ export const usersHtmlRouter = express.Router();
 usersHtmlRouter.get('/', async (req, res) => {
   try {
     const { querypage } = req.query;
-    const queryResult = await UserModel.paginate({}, { limit: 10, page: querypage || 1 });
+    const queryResult = await UserModel.paginate(
+      {
+        /* query y sort */
+      },
+      { limit: 10, page: querypage || 1 }
+    );
     let usuariosPaginados = queryResult.docs;
     const { totalDocs, limit, totalPages, page, pagingCounter, hasPrevPage, hasNextPage, prevPage, nextPage } = queryResult;
 
